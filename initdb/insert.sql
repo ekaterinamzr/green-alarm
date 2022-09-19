e2865c6e24c014e44b9acba5d9e5021cb2e2c5e5
Ekaterina <ekaterinaamzr@gmail.com>
2022-09-19 19:38:22 +0300

    delete docker files for now

--- initdb/insert.sql
+++
@@ -1,7 +0,0 @@
-COPY Types FROM '/var/lib/postgresql/csvs/types.csv' DELIMITER ';' CSV;
-COPY Roles FROM '/var/lib/postgresql/csvs/roles.csv' DELIMITER ';' CSV;
-COPY Statuses FROM '/var/lib/postgresql/csvs/statuses.csv' DELIMITER ';' CSV;
-COPY Users FROM '/var/lib/postgresql/csvs/users.csv' DELIMITER ';' CSV;
-COPY Incidents FROM '/var/lib/postgresql/csvs/incidents.csv' DELIMITER ';' CSV;
-COPY Images FROM '/var/lib/postgresql/csvs/images.csv' DELIMITER ';' CSV;
-COPY Editing_Requests FROM '/var/lib/postgresql/csvs/requests.csv' DELIMITER ';' CSV;
