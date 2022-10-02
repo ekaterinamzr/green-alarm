4970665bed38a745f39db0976b13c30e16279698
Ekaterina <ekaterinaamzr@gmail.com>
2022-10-02 10:01:08 +0300

    add some tests

---
+++ integration-tests/pgrepo_test.go
@@ -0,0 +1,52 @@
+package integrationtests
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/ekaterinamzr/green-alarm/internal/entity"
+	"github.com/ekaterinamzr/green-alarm/internal/infrastructure/pgrepo"
+	"github.com/ekaterinamzr/green-alarm/pkg/postgres"
+	"github.com/stretchr/testify/require"
+)
+
+const uri = "postgres://greenalarm:greenalarm@localhost:5432/greenalarm"
+
+func TestCreateDeleteIncident(t *testing.T) {
+	pg, err := postgres.New(uri)
+	require.NoError(t, err)
+	defer pg.Close()
+
+	repo := pgrepo.NewIncidentRepository(pg)
+
+	id, err := repo.Create(context.Background(), entity.Incident{
+		Name:        "test incident",
+		Date:        time.Now(),
+		Country:     "test country",
+		Latitude:    50,
+		Longitude:   50,
+		Publication: time.Now(),
+		Comment:     "test comment",
+		Status:      entity.Unconfirmed,
+		Type:        entity.Bio,
+		Author:      1,
+	})
+
+	require.NoError(t, err)
+
+	err = repo.Delete(context.Background(), id)
+	require.NoError(t, err)
+}
+
+func TestGetIncidents(t *testing.T) {
+	pg, err := postgres.New(uri)
+	require.NoError(t, err)
+	defer pg.Close()
+
+	repo := pgrepo.NewIncidentRepository(pg)
+	incidents, err := repo.GetAll(context.Background())
+
+	require.NoError(t, err)
+	require.NotEmpty(t, incidents)
+}
